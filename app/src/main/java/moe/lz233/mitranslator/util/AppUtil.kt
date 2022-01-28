31aa894c373f4fb2559693f4fd83662f223f7896
lz233 <lz23333@outlook.com>
2022-01-29 02:12:59 +0800

    destruct app list

--- app/src/main/java/moe/lz233/mitranslator/util/AppUtil.kt
+++ app/src/main/java/moe/lz233/mitranslator/util/AppUtil.kt
@@ -25,13 +25,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.res.Configuration
-import android.content.res.Resources
-import android.graphics.Paint
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
-import android.view.View
 import moe.lz233.mitranslator.Config
-import moe.lz233.mitranslator.util.ktx.*
 
 
 fun dp2px(dpValue: Float, context: Context = Config.context): Int = (dpValue * context.resources.displayMetrics.density + 0.5f).toInt()
@@ -45,29 +39,3 @@ fun getAppList() = mutableListOf<ApplicationInfo>().apply {
         add(Config.context.packageManager.getApplicationInfo(it.activityInfo.applicationInfo.packageName, 0))
     }
 }.toList()
-
-fun getMatureTileView(applicationInfo: ApplicationInfo) = "com.cleargrass.app.babel.launcher.TileView".findClass().new(Config.context).apply { //icon
-    setObjectField("a",PackageItemInfoCompat.loadUnbadgedIcon(applicationInfo,Config.context.packageManager))
-    val icon = getObjectField("a") as Drawable
-
-    icon.setBounds(icon.intrinsicWidth/2, icon.intrinsicHeight/2, icon.intrinsicWidth/2+50,icon.intrinsicHeight/2+50)
-    setObjectField("b", applicationInfo.loadLabel(Config.context.packageManager)) //title
-    setObjectField("c", "") //sub title
-    setIntField("d", 1) //type
-    setIntField("e", -9136724) //color 1
-    setIntField("f", -12293493) //color 2
-    setObjectField("g", Paint().apply {
-        color = -1
-        isAntiAlias = true
-    }) //paint
-
-    setObjectField("h", Paint().apply {
-        color = -2130772226
-        isAntiAlias = true
-    }) //paint
-    setObjectField("i", Paint()) //background paint
-    //click
-    //setBooleanField("k",false)
-    //??
-    setFloatField("l", 1.0f) //callMethod("onConfigurationChanged",callMethod("getResources")!!.callMethod("getConfiguration"))
-} as View
