7cba0b69666a43f6733fe8b93c80629b899665f0
lz233 <lz23333@outlook.com>
2022-01-31 00:17:28 +0800

    specify type explicitly

--- app/src/main/java/moe/lz233/mitranslator/ModuleSP.kt
+++ app/src/main/java/moe/lz233/mitranslator/ModuleSP.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 
 object ModuleSP {
-    val sp by lazy { Config.context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE) }
+    val sp: SharedPreferences by lazy { Config.context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE) }
     private val editor: SharedPreferences.Editor = sp.edit()
     fun set(key: String, any: Any) {
         when (any) {
