29fdb4b6b837397deb481e1d9b22f3fe8297bfad
lz233 <lz23333@outlook.com>
2022-06-22 16:57:04 +0800

    support i18n on label hide label when no item is shown

--- app/src/main/java/moe/lz233/mitranslator/module/AppList.kt
+++ app/src/main/java/moe/lz233/mitranslator/module/AppList.kt
@@ -1,7 +1,6 @@
 package moe.lz233.mitranslator.module
 
 import android.annotation.SuppressLint
-import android.graphics.Canvas
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,10 +11,7 @@ import moe.lz233.mitranslator.Config
 import moe.lz233.mitranslator.ModuleSP
 import moe.lz233.mitranslator.R
 import moe.lz233.mitranslator.ViewFields
-import moe.lz233.mitranslator.util.LogUtil
 import moe.lz233.mitranslator.util.getAppList
-import moe.lz233.mitranslator.util.ktx.callMethod
-import moe.lz233.mitranslator.util.ktx.getObjectField
 import moe.lz233.mitranslator.util.ktx.hookAfterMethod
 import moe.lz233.mitranslator.util.ktx.toStringList
 import org.json.JSONArray
@@ -26,21 +22,26 @@ class AppList {
         "com.cleargrass.app.babel.launcher.launch.main.MainFragment".hookAfterMethod("onViewCreated", View::class.java, Bundle::class.java) {
             val linearLayout = ViewFields.rootView.getChildAt(0) as LinearLayout
             linearLayout.addView(LayoutInflater.from(Config.context).inflate(R.layout.view_title, null))
+            //LogUtil.d(linearLayout.childCount)
             val whileListMode = ModuleSP.sp.getBoolean("whileListMode", false)
             val whileAppList = JSONArray(ModuleSP.sp.getString("whileAppList", "[]")).toStringList()
             val moreDrawable = Config.context.resources.getDrawable(2131165304, Config.context.theme)
+            var hasItem = false
             getAppList().forEach {
                 if ((!whileListMode) or (it.packageName in whileAppList)) {
+                    hasItem = true
                     linearLayout.addView(LayoutInflater.from(Config.context).inflate(R.layout.view_app, null).apply {
                         this.setOnClickListener { _ -> Config.activity.startActivity(it.launchIntent) }
                         this.findViewById<ImageView>(R.id.icon).setImageDrawable(it.icon)
                         this.findViewById<TextView>(R.id.title).text = it.name
                         this.findViewById<ImageView>(R.id.more).setImageDrawable(moreDrawable)
+                    })
                 }
             }
+            if (!hasItem) linearLayout.getChildAt(13).visibility = View.GONE
+            /*"com.cleargrass.app.babel.launcher.TileView".hookAfterMethod("onDraw", Canvas::class.java) {
+                LogUtil.d("${it.thisObject.getObjectField("b")}\n${it.thisObject.getObjectField("a")!!.callMethod("getIntrinsicWidth")}\n${it.thisObject.getObjectField("a")!!.callMethod("getIntrinsicHeight")}\n${it.thisObject.callMethod("getMeasuredHeight")}")
+            }*/
         }
-        /*"com.cleargrass.app.babel.launcher.TileView".hookAfterMethod("onDraw", Canvas::class.java) {
-            LogUtil.d("${it.thisObject.getObjectField("b")}\n${it.thisObject.getObjectField("a")!!.callMethod("getIntrinsicWidth")}\n${it.thisObject.getObjectField("a")!!.callMethod("getIntrinsicHeight")}\n${it.thisObject.callMethod("getMeasuredHeight")}")
-        }*/
     }
 }
